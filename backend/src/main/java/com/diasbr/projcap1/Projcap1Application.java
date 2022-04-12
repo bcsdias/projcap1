4f1e8408ea48d5778ee92e28dd6360d3dc51e86c
Bruno Dias <bcsdias@gmail.com>
2022-04-12 20:31:47 -0300

    Backend project created

---
+++ backend/src/main/java/com/diasbr/projcap1/Projcap1Application.java
@@ -0,0 +1,13 @@
+package com.diasbr.projcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projcap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diasbr/projcap1/Projcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diasbr.projcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
