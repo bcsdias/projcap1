60b0c9ae9b174de074a56ef2c86e6c94245e56c0
Bruno Dias <bcsdias@gmail.com>
2022-04-12 21:55:14 -0300

    Client and ClientResourse

--- backend/src/main/java/com/diasbr/projcap1/resources/ClientResource.java
+++ backend/src/main/java/com/diasbr/projcap1/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.diasbr.projcap1.entities.Client;
 
 @RestController //informar a annotation para configurar o controlador REST
